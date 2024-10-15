44d055909074cbb741dc32203b7914c586ef411c
artsyl3 <118113234+artStyl3@users.noreply.github.com>
2024-10-15 20:55:37 +0200

    changes

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const postman = require('postman-request');
 
 const url = 'http://api.weatherstack.com/current?access_key=123b85c29987d638ffa8a55e92a94a40&query=Tehran';
 
-postman({url:url} , (error, response) =>{
-    const data = response.body;
+postman({url:url, json:true} , (error, response) =>{
+    const data = response.body.current;
     if(error){
         return console.log(error);
     }else{
-        console.log(JSON.parse(data));
+        console.log("It is currently " + data.temperature + " degress out. It feels like " + data.feelslike + " degress out.");
     }
 });
 
