f7ea0381c86f475c92ae907290d2e564c15fdf82
artsyl3 <118113234+artStyl3@users.noreply.github.com>
2024-10-15 20:43:04 +0200

    asd

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const postman = require('postman-request');
 
 const url = 'http://api.weatherstack.com/current?access_key=123b85c29987d638ffa8a55e92a94a40&query=Tehran';
 
-postman({url:url} , function(error, response){
+postman({url:url} , (error, response) =>{
     const data = response.body;
     if(error){
         return console.log(error);
     }else{
         console.log(JSON.parse(data));
     }
-})
+});
+
