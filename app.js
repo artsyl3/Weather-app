b9e2c45838a717c68deb2d3f61874e6a9c35381d
artsyl3 <118113234+artStyl3@users.noreply.github.com>
2024-10-21 00:04:04 +0200

    last changes

--- app.js
+++ app.js
@@ -1,25 +1,26 @@
 const postman = require('postman-request');
 
 
-// const url = 'http://api.weatherstack.com/current?access_key=123b85c29987d638ffa8a55e92a94a40&query=Tehran&units=f';
+const url = 'http://api.weatherstack.com/current?access_key=123b85c29987d638ffa8a55e92a94a40&query=Tehran&units=f';
 
-// postman({url:url, json:true} , (error, response) =>{
-//     const data = response.body.current;
-//     if(error){
-//         return console.log(error);
-//     }else{
-//         console.log(data.weather_descriptions[0] + ". it is currently " + data.temperature + " degress out. It feels like " + data.feelslike + " degress out.");
-//     }
-// });
+postman({url:url, json:true} , (error, response) =>{
+    const data = response.body.current;
+    if(error){
+        return console.log(error);
+    }else{
+        console.log(data.weather_descriptions[0] + ". it is currently " + data.temperature + " degress out. It feels like " + data.feelslike + " degress out.");
+    }
+});
 
 const geocodeUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiZ2hhbmJhcmkyIiwiYSI6ImNtMmc0Y3drdjBobnIyanBmd3F2NjdtMW8ifQ.SVzv1P-BreOYxZ0XU4PZ4A&limit=1';
 
-postman({url: geocoderl , json:true}, (error, response) => {
+postman({url: geocodeUrl , json:true}, (error, response) => {
     const data2 = response;
     if(error) {
         return console.log(error);
     }else {
-        
-        return console.log(data2.body.features[0].center);
+        const latitude = data2.body.features[0].center[1];
+        const longitude = data2.body.features[0].center[0];
+        return console.log(latitude, longitude);
     }
 })
