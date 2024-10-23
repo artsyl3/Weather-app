85e3f15b11ef5d74ef79080ecaea361cf1c792da
artsyl3 <118113234+artStyl3@users.noreply.github.com>
2024-10-23 18:23:12 +0200

    jj

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
 const postman = require('postman-request');
 
 
-// const url = 'http://api.weatherstack.com/current?access_key=cc4e253f5df50564c454e6fec5be3193&query=Tehran&units=f';
+const url = 'http://api.weatherstack.com/current?access_key=cc4e253f5df50564c454e6fec5be3193&query=Tehran&units=f';
 
-// postman({url:url, json:true} , (error, response) =>{
-//     const data = response.body;
-//     if(error){
-//         return console.log("Nemidonam chi shode");
-//     }else{
-//         console.log(data.weather_descriptions[0] + ". it is currently " + data.temperature + " degress out. It feels like " + data.feelslike + " degress out.");
-//         console.log(data);
-//     }
-// });
+postman({url:url, json:true} , (error, response) =>{
+    const data = response.body.current;
+    if(error){
+        return console.log("Nemidonam chi shode");
+    }else{
+         console.log(data.weather_descriptions[0] + ". it is currently " + data.temperature + " degress out. It feels like " + data.feelslike + " degress out.");
+    }
+});
 
 const geocodeUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json.json?access_token=pk.eyJ1IjoiZ2hhbmJhcmkyIiwiYSI6ImNtMmc0Y3drdjBobnIyanBmd3F2NjdtMW8ifQ.SVzv1P-BreOYxZ0XU4PZ4A&limit=1';
 
